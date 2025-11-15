73ed7ebcc36059121000102d197f79b3229b3a18
Carlos <U202015274@upc.edu.pe>
2025-11-15 01:14:05 -0500

    fix(auth): save token to tokenstorage

--- src/infrastructure/api/repositories/AuthRepositoryImpl.js
+++ src/infrastructure/api/repositories/AuthRepositoryImpl.js
@@ -1,21 +1,19 @@
 import { AuthRepository } from "../../../domain/repositories/AuthRepository";
 import httpClient from '../config/httpClient.js';
 import { API_ENDPOINTS } from '../config/apiConfig.js';
+import { TokenStorage } from '../../storage/TokenStorage.js'; 
 import { User } from '../../../domain/entities/User.js';
 
 export class AuthRepositoryImpl extends AuthRepository {
 
     async register(userData) {
         try {
-
             const response = await httpClient.post(
                 API_ENDPOINTS.AUTH.REGISTER,
                 userData
             );
-
             return response.data;
         } catch (error) {
-
             throw error;
         }
     }
@@ -27,8 +25,17 @@ export class AuthRepositoryImpl extends AuthRepository {
                 { email, password }
             );
 
+            const token = response.data.token || response.data.accessToken;
+            const refreshToken = response.data.refreshToken;
+            const user = response.data.user;
+
+            if (token) TokenStorage.setAccessToken(token);
+            if (refreshToken) TokenStorage.setRefreshToken(refreshToken);
+            if (user) TokenStorage.setUserData(user);
+
             return response.data;
         } catch (error) {
+            console.error("[AuthRepositoryImpl] Login error:", error);
             throw error;
         }
     }
@@ -40,6 +47,7 @@ export class AuthRepositoryImpl extends AuthRepository {
                 { refreshToken }
             );
 
+            TokenStorage.clear();
             return response.data;
         } catch (error) {
             throw error;
@@ -53,6 +61,11 @@ export class AuthRepositoryImpl extends AuthRepository {
                 { refreshToken }
             );
 
+            const { accessToken, refreshToken: newRefreshToken } = response.data;
+
+            if (accessToken) TokenStorage.setAccessToken(accessToken);
+            if (newRefreshToken) TokenStorage.setRefreshToken(newRefreshToken);
+
             return response.data;
         } catch (error) {
             throw error;
@@ -62,7 +75,6 @@ export class AuthRepositoryImpl extends AuthRepository {
     async getCurrentUser() {
         try {
             const response = await httpClient.get(API_ENDPOINTS.AUTH.GET_PROFILE);
-
             return User.fromBackend(response.data);
         } catch (error) {
             throw error;
@@ -71,8 +83,7 @@ export class AuthRepositoryImpl extends AuthRepository {
 
     async getAllUsers() {
         try {
-            const response = await httpClient.get(API_ENDPOINTS.AUTH.ALL);
-
+            const response = await httpClient.get(API_ENDPOINTS.AUTH.GET_ALL);
             return User.fromBackendArray(response.data);
         } catch (error) {
             throw error;
@@ -82,13 +93,11 @@ export class AuthRepositoryImpl extends AuthRepository {
     async getUserById(id) {
         try {
             const response = await httpClient.get(API_ENDPOINTS.AUTH.BY_ID(id));
-
             return User.fromBackend(response.data);
         } catch (error) {
             throw error;
         }
     }
-
 }
 
 export const authRepository = new AuthRepositoryImpl();
